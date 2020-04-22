0588ba488d1b8d29c5f4a7df7cd51eee432b07b5
sfctl <catalbasifa@gmail.com>
2020-04-22 23:07:09 +0300

    Initial Commit

---
+++ readSpeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  readSpeed
+//
+//  Created by Yasemin YEL on 22.04.2020.
+//  Copyright © 2020 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
